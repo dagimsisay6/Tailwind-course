127fc181bb4e010c0997a358b32ab3ddaac8139d
Dagim Sisay <Dagim>
2025-06-27 22:49:35 +0300

    Added a night/dark mode and completed the chapters

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  darkMode: "class",
+  content: ["./*.html", "./js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        widescreen: { raw: "(min-aspect-ratio: 3/2)" },
+        tallscreen: { raw: "(min-aspect-ratio: 13/20)" },
+      },
+    },
   },
   plugins: [],
 };
